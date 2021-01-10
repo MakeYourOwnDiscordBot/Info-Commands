7076581e6e3ae592075f6fd90e82a69db27772af
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2021-01-10 16:41:29 +0900

    Update role.js

--- info/commands/role.js
+++ info/commands/role.js
@@ -5,11 +5,9 @@ const { MessageEmbed } = require('discord.js');
 const config = require('./../commandConfig')
 const co = config.roleCommand;
 module.exports = {
-      name:'role',
-			aliases:co.aliases,
-			description: 'ロール情報を表示\n使用方法: !role <@role>\n!role <role ID>',
-			
-
+      	name:'role',
+	aliases:co.aliases,
+	description: 'ロール情報を表示\n使用方法: !role <@role>\n!role <role ID>',
 
 	execute(msg,args,client) {
     	const role = msg.guild.roles.cache.get(args[0]) || msg.mentions.roles.first();
@@ -21,7 +19,7 @@ module.exports = {
 			.addField('作成日', moment(role.createdAt).tz("Asia/Tokyo").format('MM/DD/YYYY h:mm A'), true)
 			.addField('表示役職？', role.hoist ? 'Yes' : 'No', true)
 			.addField('メンション可能？', role.mentionable ? 'Yes' : 'No', true)
-      .addField('権限', role.permissions.toArray()|| 'None');
+      co.showPermission ? embed.addField('権限', role.permissions.toArray()|| 'None') : null;
 	msg.channel.send(embed);
 	}
 };
