4ee732ee83cc48e3b4e871ca97b577125567c808
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2021-01-10 17:05:16 +0900

    Update role.js

--- info/commands/role.js
+++ info/commands/role.js
@@ -5,11 +5,15 @@ const { MessageEmbed } = require('discord.js');
 const config = require('./../commandConfig')
 const co = config.roleCommand;
 module.exports = {
-      	name:'role',
-	aliases:co.aliases,
-	description: 'ロール情報を表示\n使用方法: !role <@role>\n!role <role ID>',
+      name:'role',
+      aliases:co.aliases,
+      cooldown:co.cooldown,
+      description: co.description,
+      ownerOnly:co.ownerOnly,
+      disabled:co.disabled,
 
-	execute(msg,args,client) {
+	async execute(msg,args,client) {
+    const mesmeber = await msg.guild.members.fetch(msg.author.id);
     	const role = msg.guild.roles.cache.get(args[0]) || msg.mentions.roles.first();
 		const embed = new MessageEmbed()
 			.setColor(role.hexColor)
@@ -19,7 +23,9 @@ module.exports = {
 			.addField('作成日', moment(role.createdAt).tz("Asia/Tokyo").format('MM/DD/YYYY h:mm A'), true)
 			.addField('表示役職？', role.hoist ? 'Yes' : 'No', true)
 			.addField('メンション可能？', role.mentionable ? 'Yes' : 'No', true)
-      co.showPermission ? embed.addField('権限', role.permissions.toArray()|| 'なし') : null;
+      if(mesmeber.hasPermission("ADMINISTRATOR")){
+      args[1] === "true" ? embed.addField('権限', role.permissions.toArray()|| 'None') : null;
+      }
 	msg.channel.send(embed);
 	}
 };
